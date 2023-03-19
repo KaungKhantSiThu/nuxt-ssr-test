2bbe5cb95d2d3ef9b20309d9588257add2fb6652
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-03-20 00:09:02 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <NavigationBar />
+    <Nuxt />
+  </div>
+</template>
+
+<script setup>
+import NavigationBar from '~/components/NavigationBar.vue'
+</script>
+
+
